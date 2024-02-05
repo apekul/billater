c970c2387ca4b1f5e5c2f1ea5bcfae1b03b45160
Adaś <adampekul@gmail.com>
2024-02-05 16:01:38 +0100

    add simple function to create new events, add scrollable view

--- Components/EventList.js
+++ Components/EventList.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   Text,
   SafeAreaView,
+  ScrollView,
   Alert,
   StyleSheet,
   TouchableHighlight,
@@ -10,14 +11,28 @@ import {
 import IconAnt from "react-native-vector-icons/AntDesign";
 import IconMat from "react-native-vector-icons/MaterialIcons";
 
+const fakeItems = [
+  { id: 1, title: "Beer", ppl: 3, value: {}, date: "02.02.2024" },
+  { id: 2, title: "Kebab", ppl: 3, value: {}, date: "01.02.2024" },
+  { id: 3, title: "Food", ppl: 3, value: {}, date: "10.01.2024" },
+  { id: 4, title: "Food", ppl: 3, value: {}, date: "10.01.2024" },
+];
 const EventList = () => {
-  const fakeItems = [
-    { id: 1, title: "Beer", ppl: 3, value: {}, date: "02.02.2024" },
-    { id: 2, title: "Kebab", ppl: 3, value: {}, date: "01.02.2024" },
-    { id: 3, title: "Food", ppl: 3, value: {}, date: "10.01.2024" },
-    { id: 4, title: "Food", ppl: 3, value: {}, date: "10.01.2024" },
-  ];
-  const groupedItems = fakeItems.reduce((acc, event) => {
+  const [events, setEvents] = useState(fakeItems);
+
+  const addNewEvent = () => {
+    const newID = Math.max(events.map((v) => v.id)) + 1;
+    const newItem = {
+      id: newID,
+      title: "sampleName",
+      ppl: 3,
+      value: {},
+      date: new Date(),
+    };
+    setEvents((prev) => [...prev, newItem]);
+  };
+
+  const groupedItems = events.reduce((acc, event) => {
     const date = event.date;
 
     if (!acc[date]) {
@@ -36,64 +51,40 @@ const EventList = () => {
         underlayColor="#DDDDDD"
         style={[styles.btn]}
         onPress={() => {
-          return Alert.alert("CLiced add Event");
+          return addNewEvent();
         }}
       >
         <IconMat name="add-box" size={40} color="#898A8D" />
       </TouchableHighlight>
       {/* Event */}
-      {Object.entries(groupedItems).map(([date, events], i) => (
-        <View key={i} style={{ marginBottom: 10 }}>
-          <View style={styles.dateBar}>
-            <Text style={{ paddingHorizontal: 10, fontWeight: "bold" }}>
-              {date}
-            </Text>
-          </View>
-
-          {events.map((event, j) => (
-            <View key={j} style={[styles.group, { marginVertical: 10 }]}>
-              <View style={[styles.group, { gap: 10 }]}>
-                <View style={styles.icon}></View>
-                <View>
-                  <Text style={{ fontWeight: "bold" }}>{event.title}</Text>
-                  <Text>{event.ppl} participant</Text>
-                </View>
-              </View>
-
-              <View>
-                <Text>balance</Text>
-                <Text>$20</Text>
-              </View>
-            </View>
-          ))}
-        </View>
-      ))}
-      {/* <View>
-        {fakeItems.map((event, i) => (
-          <View style={{ marginBottom: 10 }} key={event.id}>
+      <ScrollView>
+        {Object.entries(groupedItems).map(([date, events], i) => (
+          <View key={i} style={{ marginBottom: 10 }}>
             <View style={styles.dateBar}>
               <Text style={{ paddingHorizontal: 10, fontWeight: "bold" }}>
-                {event.date}
+                {date}
               </Text>
             </View>
 
-            <View style={[styles.group, { marginVertical: 10 }]}>
-              <View style={[styles.group, { gap: 10 }]}>
-                <View style={styles.icon}></View>
+            {events.map((event, j) => (
+              <View key={j} style={[styles.group, { marginVertical: 10 }]}>
+                <View style={[styles.group, { gap: 10 }]}>
+                  <View style={styles.icon}></View>
+                  <View>
+                    <Text style={{ fontWeight: "bold" }}>{event.title}</Text>
+                    <Text>{event.ppl} participant</Text>
+                  </View>
+                </View>
+
                 <View>
-                  <Text style={{ fontWeight: "bold" }}>{event.title}</Text>
-                  <Text>{event.ppl} participant</Text>
+                  <Text>balance</Text>
+                  <Text>$20</Text>
                 </View>
               </View>
-
-              <View>
-                <Text>balance</Text>
-                <Text>$20</Text>
-              </View>
-            </View>
+            ))}
           </View>
         ))}
-      </View> */}
+      </ScrollView>
     </View>
   );
 };
